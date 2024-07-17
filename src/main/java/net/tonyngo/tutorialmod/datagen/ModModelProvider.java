aedadae825d07341e2bddbf59f683994ae67f67a
tonyngo6 <tonyngot@gmail.com>
2024-07-17 00:32:25 -0700

    fixed ModModelProvider

--- src/main/java/net/tonyngo/tutorialmod/datagen/ModModelProvider.java
+++ src/main/java/net/tonyngo/tutorialmod/datagen/ModModelProvider.java
@@ -2,13 +2,13 @@ package net.tonyngo.tutorialmod.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
-import net.tonyngo.tutorialmod.block.ModBlocks;
-import net.tonyngo.tutorialmod.block.custom.TomatoCropBlock;
-import net.tonyngo.tutorialmod.item.ModItems;
 import net.minecraft.data.client.BlockStateModelGenerator;
 import net.minecraft.data.client.ItemModelGenerator;
 import net.minecraft.data.client.Models;
-import net.minecraft.item.ArmorItem;
+import net.tonyngo.tutorialmod.block.ModBlocks;
+import net.tonyngo.tutorialmod.block.custom.TomatoCropBlock;
+import net.tonyngo.tutorialmod.item.custom.ModItems;
+
 
 public class ModModelProvider extends FabricModelProvider {
     public ModModelProvider(FabricDataOutput output) {
@@ -17,47 +17,12 @@ public class ModModelProvider extends FabricModelProvider {
 
     @Override
     public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
-        BlockStateModelGenerator.BlockTexturePool rubyPool = blockStateModelGenerator.registerCubeAllModelTexturePool(ModBlocks.RUBY_BLOCK);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.RAW_RUBY_BLOCK);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.RUBY_ORE);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.DEEPSLATE_RUBY_ORE);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.NETHER_RUBY_ORE);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.END_STONE_RUBY_ORE);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.SOUND_BLOCK);
-
-        rubyPool.stairs(ModBlocks.RUBY_STAIRS);
-        rubyPool.slab(ModBlocks.RUBY_SLAB);
-        rubyPool.button(ModBlocks.RUBY_BUTTON);
-        rubyPool.pressurePlate(ModBlocks.RUBY_PRESSURE_PLATE);
-        rubyPool.fence(ModBlocks.RUBY_FENCE);
-        rubyPool.fenceGate(ModBlocks.RUBY_FENCE_GATE);
-        rubyPool.wall(ModBlocks.RUBY_WALL);
-
-        blockStateModelGenerator.registerDoor(ModBlocks.RUBY_DOOR);
-        blockStateModelGenerator.registerTrapdoor(ModBlocks.RUBY_TRAPDOOR);
-
         blockStateModelGenerator.registerCrop(ModBlocks.TOMATO_CROP, TomatoCropBlock.AGE, 0, 1, 2, 3, 4, 5);
     }
 
     @Override
     public void generateItemModels(ItemModelGenerator itemModelGenerator) {
-        itemModelGenerator.register(ModItems.RUBY, Models.GENERATED);
-        itemModelGenerator.register(ModItems.RAW_RUBY, Models.GENERATED);
-
-        itemModelGenerator.register(ModItems.COAL_BRIQUETTE, Models.GENERATED);
         itemModelGenerator.register(ModItems.TOMATO, Models.GENERATED);
-        itemModelGenerator.register(ModItems.METAL_DETECTOR, Models.GENERATED);
-
-        itemModelGenerator.register(ModItems.RUBY_PICKAXE, Models.HANDHELD);
-        itemModelGenerator.register(ModItems.RUBY_AXE, Models.HANDHELD);
-        itemModelGenerator.register(ModItems.RUBY_SHOVEL, Models.HANDHELD);
-        itemModelGenerator.register(ModItems.RUBY_SWORD, Models.HANDHELD);
-        itemModelGenerator.register(ModItems.RUBY_HOE, Models.HANDHELD);
-
-        itemModelGenerator.registerArmor(((ArmorItem) ModItems.RUBY_HELMET));
-        itemModelGenerator.registerArmor(((ArmorItem) ModItems.RUBY_CHESTPLATE));
-        itemModelGenerator.registerArmor(((ArmorItem) ModItems.RUBY_LEGGINGS));
-        itemModelGenerator.registerArmor(((ArmorItem) ModItems.RUBY_BOOTS));
-
     }
 }
+
